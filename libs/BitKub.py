5e095d804fde701da097221ccb846b1da78a26cf
Taweechai Yuenyang <krumii.it@gmail.com>
2022-03-11 11:17:56 +0700

    update get cost

--- libs/BitKub.py
+++ libs/BitKub.py
@@ -1,10 +1,97 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
+import requests
+import json
+import hmac
+import hashlib
+from datetime import datetime
+
 
 class BitKub:
     def __init__(self):
         # Initial envs.
-        self.API_HOST= os.getenv('BITKUB_HOST')
-        self.API_KEY= os.getenv('BITKUB_KEY')
-        self.API_SECRET= os.getenv('BITKUB_SECRET')
+        self.API_HOST = os.getenv('BITKUB_HOST')
+        self.API_KEY = os.getenv('BITKUB_KEY')
+        self.API_SECRET = os.getenv('BITKUB_SECRET')
+        self.API_HEADER = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-BTK-APIKEY': self.API_KEY,
+        }
+
+        # initialize timeframe
+        # self.INTERVAL_1_MINUTE = "1m"
+        # self.INTERVAL_5_MINUTES = "5m"
+        self.INTERVAL_15_MINUTES = "15m"
+        self.INTERVAL_30_MINUTES = "30m"
+        self.INTERVAL_1_HOUR = "1h"
+        self.INTERVAL_2_HOURS = "2h"
+        self.INTERVAL_4_HOURS = "4h"
+        self.INTERVAL_1_DAY = "1d"
+        # self.INTERVAL_1_WEEK = "1W"
+        # self.INTERVAL_1_MONTH = "1M"
+
+    def timeframe(self):
+        return [
+            # self.INTERVAL_1_MINUTE,
+            # self.INTERVAL_5_MINUTES,
+            self.INTERVAL_15_MINUTES,
+            self.INTERVAL_30_MINUTES,
+            self.INTERVAL_1_HOUR,
+            self.INTERVAL_2_HOURS,
+            self.INTERVAL_4_HOURS,
+            self.INTERVAL_1_DAY,
+            # self.INTERVAL_1_WEEK,
+            # self.INTERVAL_1_MONTH,
+        ]
+        
+
+    def json_encode(data):
+        return json.dumps(data, separators=(',', ':'), sort_keys=True)
+
+    def sign(self, data):
+        j = self.json_encode(data)
+        # print('Signing payload: ' + j)
+        h = hmac.new(self.API_SECRET, msg=j.encode(), digestmod=hashlib.sha256)
+        return h.hexdigest()
+
+    def timestamps(self):
+        url = f"{self.API_HOST}/api/servertime"
+
+        payload = {}
+        headers = {}
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        return {
+            'timestamp': response.text,
+            'datetime': datetime.fromtimestamp(int(response.text))
+        }
+        
+    def ticket(self, product='BTC'):
+        ticker = requests.get(self.API_HOST + '/api/market/ticker')
+        ticker = ticker.json()
+        return ticker[f'THB__{product}']
+
+    def symbols(self):
+        url = f"{self.API_HOST}/api/market/symbols"
+
+        payload = {}
+        headers = {}
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        doc = []
+        obj = response.json()
+        if obj['error'] == 0:
+            data = obj['result']
+            for i in data:
+                doc.append(str(i['symbol'])[4:])
+
+        doc.sort()
+        return doc
+    
+    def price(self, product='BTC'):
+        ticker = requests.get(self.API_HOST + '/api/market/ticker')
+        ticker = ticker.json()
+        price = float(ticker[f'THB__{product}']['last'])
+        return price
--- main.py
+++ main.py
@@ -1,12 +1,66 @@
-import os
 import sys
+from termcolor import colored
+from tradingview_ta import TA_Handler, Interval, Exchange
+from dotenv import load_dotenv
+
+load_dotenv('./env')
 from libs.BitKub import BitKub
 
 # initialize environ
 bitkub = BitKub()
 
+
 def main():
-    print(bitkub.API_HOST)
+    server_time = bitkub.timestamps()
+    print(
+        colored(f"start run datetime on server: {server_time['datetime']}",
+                "red"))
+
+    symbols = bitkub.symbols()
+    for s in symbols:
+        # loop timeframe
+        score = 0
+        print(colored(f"start loop {s}", "blue"))
+        for t in bitkub.timeframe():
+            ta = TA_Handler(symbol=f"{s}THB",
+                            screener="crypto",
+                            exchange="Bitkub",
+                            interval=t)
+            summary = []
+            try:
+                summary = ta.get_analysis().summary
+                summary['SYMBOL'] = s
+                summary['QOUTE'] = "THB"
+                summary['ON_TIME'] = t
+            except:
+                pass
+
+            if len(summary) > 0:
+                recomm = summary['RECOMMENDATION']
+                x = 0
+                if str(recomm).find('BUY') >= 0: x = 1
+                if recomm == "NEUTRAL": x = 1
+                txt_color = "green"
+                if x == 0: txt_color = "red"
+
+                print(f"{s} is {colored(recomm, txt_color)} on {t} score: {x}")
+                score += x
+
+        print(colored(f"end loop {s}", "blue"))
+        interesting = "Sell"
+        txt_color = "red"
+        if score >= len(bitkub.timeframe()):
+            interesting = "Buy"
+            txt_color = "green"
+
+        print(
+            f"{s} is {colored(interesting, txt_color)}({score}-{len(bitkub.timeframe())} = {colored(score-len(bitkub.timeframe()), txt_color)}) price: {bitkub.price(product=s)}"
+        )
+        
+        if interesting == "Buy":
+            print('insert data')
+            
+        print("******************************")
 
 
 if __name__ == '__main__':
