f3ef32d0bc354ff1f5817f01d9e77d2c0db7c920
Taweechai Yuenyang <krumii.it@gmail.com>
2022-03-12 16:21:29 +0700

    fix forget check trend

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
     for s in symbols:
         print(colored(f"start new order loop {s}", "green"))
         x = bitkub.check_trend(symbol=s)
-        if x['interesting']:
+        if x['trend'] == 'Buy' and x['interesting'] is True:
             mydb.insert(symbol=x['symbol'],
                         price=x['price'],
                         percent=x['percent'],
