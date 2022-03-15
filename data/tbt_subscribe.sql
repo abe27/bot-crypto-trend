350439ba14c7261ff0c2bc4479c43af0d6e0023c
Taweechai Yuenyang <krumii.it@gmail.com>
2022-03-15 13:41:35 +0700

    add new env

--- data/tbt_subscribe.sql
+++ data/tbt_subscribe.sql
@@ -1,11 +1,11 @@
 -- trend_db.tbt_subscribe definition
 
-CREATE TABLE `tbt_subscribe` (
+CREATE TABLE `tbt_investment` (
   `id` char(21) NOT NULL,
   `momemtum` char(25) NOT NULL,
   `symbol` char(10) NOT NULL,
-  `on_price` double NOT NULL,
-  `on_percent` double NOT NULL,
+  `price` double NOT NULL,
+  `percent` double NOT NULL,
   `last_price` double DEFAULT 0,
   `percent_change` double DEFAULT 0,
   `is_trend` tinyint(1) DEFAULT 0,
