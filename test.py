f8cd25dc3feef5669d45c50c7b915f8d798f8ee0
Taweechai Yuenyang <krumii.it@gmail.com>
2022-03-16 13:25:03 +0700

    test

--- test.py
+++ test.py
@@ -1,10 +1,24 @@
-import sys
-from libs.Logging import Logging
+#importing variables
+import pandas as pd
+import numpy as np
+import datetime as dt
+import pandas_datareader as pdr
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-def main():
-    Logging(symbol='NEAR', msg='CLOSED BTC')
-    
-if __name__ == '__main__':
-    main()
-    sys.exit(0)
-    
+#extracting data from Yahoo Finance API
+tickers = ['AAPL','NFLX']
+all_data = pd.DataFrame()
+test_data = pd.DataFrame()
+no_data = []
+
+for i in tickers:
+    try:
+        test_data = pdr.get_data_yahoo(i, start = dt.datetime(1990,1,1), end = dt.date.today())
+        test_data['symbol'] = i
+        all_data = all_data.append(test_data)
+    except:
+        no_data.append(i)
+
+#Creating Return column
+all_data['return'] = all_data.groupby('symbol')['Close'].pct_change() 
