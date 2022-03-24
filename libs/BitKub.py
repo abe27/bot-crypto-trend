b5830d451ccedf1e5e6f92dfd7a4ce93b902b309
Taweechai Yuenyang <krumii.it@gmail.com>
2022-03-24 08:04:17 +0700

    เปลี่ยนเวลา

--- libs/BitKub.py
+++ libs/BitKub.py
@@ -37,7 +37,7 @@ class BitKub:
             # self.INTERVAL_1_MINUTE,
             # self.INTERVAL_5_MINUTES,
             # self.INTERVAL_15_MINUTES,
-            self.INTERVAL_30_MINUTES,
+            # self.INTERVAL_30_MINUTES,
             self.INTERVAL_1_HOUR,
             self.INTERVAL_2_HOURS,
             self.INTERVAL_4_HOURS,
