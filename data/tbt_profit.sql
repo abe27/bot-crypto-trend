ceceda7f528b53a90a60c9959f532ce551242165
Taweechai Yuenyang <krumii.it@gmail.com>
2022-03-30 16:18:47 +0700

    update profit

---
+++ data/tbt_profit.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `tbt_profits` (
+  `id` char(21) NOT NULL,
+  `exchange` char(50) NOT NULL,
+  `momentum` char(25) NOT NULL,
+  `symbol` char(10) NOT NULL,
+  `quotes` char(10) NOT NULL,
+  `price` double NOT NULL,
+  `percent` double NOT NULL,
+  `last_price` double DEFAULT 0,
+  `percent_change` double DEFAULT 0,
+  `is_trend` tinyint(1) DEFAULT 0,
+  `avg_score` decimal(10,2) DEFAULT 0.00,
+  `is_activate` tinyint(1) DEFAULT 1,
+  `created_on` datetime DEFAULT current_timestamp(),
+  `last_update` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
