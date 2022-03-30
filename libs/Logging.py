b82c7c3b529624ea2e4a0c5a80279e9474d9d192
Taweechai Yuenyang <krumii.it@gmail.com>
2022-03-30 12:16:18 +0700

    แก้ไข log

--- libs/Logging.py
+++ libs/Logging.py
@@ -6,7 +6,7 @@ class Logging:
         dte = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         symb = str(f'[{symbol}]').ljust(10)
         txt = f'{dte} {symb} {msg}'
-        filename = f'bot-{exchange}-{datetime.now().strftime("%Y-%m-%d")}.log'
+        filename = f'bot-{exchange}-{symbol}-{datetime.now().strftime("%Y-%m-%d")}.log'
         
         if os.path.exists('logs') is False:
             os.mkdir('logs')
