6b645e6927014eeeeaae4ec91282e87c07ae2892
Taweechai Yuenyang <krumii.it@gmail.com>
2022-04-01 09:45:04 +0700

    update check trend

--- libs/Trend.py
+++ libs/Trend.py
@@ -56,14 +56,14 @@ class Trend:
         trend = False
         score = 0
         obj_trend = []
-        check_lower_profit = -1
-        check_top_profit = 1
+        # check_lower_profit = -1
+        # check_top_profit = 1
         ### ตึงราคาและเปอร์เซนต์การเปลี่ยนแปลงล่าสุด
         last_price = self.price(exchange=exchange,
                                 symbol=symbol,
                                 quotes=quotes)
         ### loop ด้วย timeframe
-        time_array = ["1h", "2h", "4h", "1d", "1W", "1M"]
+        # time_array = ["1h", "2h", "4h", "1d", "1W", "1M"]
         for t in TimeFrame().timeframe():
             ### ตรวจสอบ trend จาก web tradingview
             ta = TA_Handler(symbol=f"{symbol}{quotes}",
@@ -86,73 +86,74 @@ class Trend:
 
                 # print(recommendation)
                 summ = recommendation['RECOMMENDATION']
+                if summ == "STRONG_SELL" or str(summ).find("BUY") >= 0:
+                    trend = True
             except:
                 pass
 
-            obj_trend.append(summ)
-            x = 0
-            txt_color = "red"
-            ### กรอง recomment ที่เป็น strong sell
-            txt_time = "STRONG_BUY"
-            time_match = t in time_array
-            if time_match:
-                txt_time = "BUY"
-                if last_price[1] > check_lower_profit and last_price[
-                        1] < check_top_profit:
-                    txt_time = "STRONG_BUY"
+        #     obj_trend.append(summ)
+        #     x = 0
+        #     txt_color = "red"
+        #     ### กรอง recomment ที่เป็น strong sell
+        #     txt_time = "STRONG_BUY"
+        #     time_match = t in time_array
+        #     if time_match:
+        #         txt_time = "BUY"
+        #         if last_price[1] > check_lower_profit and last_price[
+        #                 1] < check_top_profit:
+        #             txt_time = "STRONG_BUY"
 
-            if str(summ) == "STRONG_SELL" or str(summ).find(txt_time) == 0:
-                x = 1
-                txt_color = "green"
+        #     if str(summ) == "STRONG_SELL" or str(summ).find(txt_time) == 0:
+        #         x = 1
+        #         txt_color = "green"
 
-            print(
-                f"{symbol} {momentum}: {colored(summ, txt_color)} ON:{t} SCORE: {x}"
-            )
-            ### ทำคะแนน avg
-            score += x
+        #     print(
+        #         f"{symbol} {momentum}: {colored(summ, txt_color)} ON:{t} SCORE: {x}"
+        #     )
+        #     ### ทำคะแนน avg
+        #     score += x
 
-        ### ตึงราคาและเปอร์เซนต์การเปลี่ยนแปลงล่าสุด
-        last_price = self.price(exchange=exchange,
-                                symbol=symbol,
-                                quotes=quotes)
-        if market == "SPOT":
-            interesting = "Sell"
-            txt_color = "red"
-            total_timeframe = len(TimeFrame().timeframe())
-            ### ตรวจสอบคะแนน avg > timeframe.length
-            if score >= len(
-                    TimeFrame().timeframe()) or (score - total_timeframe) >= 0:
-                interesting = "Buy"
-                txt_color = "green"
-                # trend = True
+        # ### ตึงราคาและเปอร์เซนต์การเปลี่ยนแปลงล่าสุด
+        # last_price = self.price(exchange=exchange,
+        #                         symbol=symbol,
+        #                         quotes=quotes)
+        # if market == "SPOT":
+        #     interesting = "Sell"
+        #     txt_color = "red"
+        #     total_timeframe = len(TimeFrame().timeframe())
+        #     ### ตรวจสอบคะแนน avg > timeframe.length
+        #     if score >= len(TimeFrame().timeframe()) or (score - total_timeframe) >= 0:
+        #         interesting = "Buy"
+        #         txt_color = "green"
+        #         # trend = True
 
-            ### ตรวจสอบราคาล่าสุด
-            if last_price[0] == 0:
-                interesting = "-"
-                txt_color = "magenta"
+        #     ### ตรวจสอบราคาล่าสุด
+        #     if last_price[0] == 0:
+        #         interesting = "-"
+        #         txt_color = "magenta"
 
-            price = f"{last_price[0]:,}"
-            # # ### ตรวจสอบเปอร์เซนต์การเปลี่ยนแปลงต้อง < 0 กำหนดเป็นขาขึ้น
-            txt_msg = "ขาลง 👇"
-            if str(summ) == "STRONG_SELL":
-                # trend = False
-                # profit_limit = float(os.getenv('STRONG_BNB_PERCENT', 10))
-                # positive_limit = profit_limit * (-1)
-                if interesting == "Buy" and last_price[1] <= neg_positive_limit:
-                    trend = True
-                    txt_msg = "ขาขึ้น ☝️"
+        #     price = f"{last_price[0]:,}"
+        #     # # ### ตรวจสอบเปอร์เซนต์การเปลี่ยนแปลงต้อง < 0 กำหนดเป็นขาขึ้น
+        #     txt_msg = "ขาลง 👇"
+        #     if str(summ) == "STRONG_SELL":
+        #         # trend = False
+        #         # profit_limit = float(os.getenv('STRONG_BNB_PERCENT', 10))
+        #         # positive_limit = profit_limit * (-1)
+        #         if interesting == "Buy" and last_price[1] <= neg_positive_limit:
+        #             trend = True
+        #             txt_msg = "ขาขึ้น ☝️"
 
-            elif str(summ).find('BUY') >= 0:
-                if interesting == "Buy":
-                    if last_price[1] > check_lower_profit and last_price[
-                            1] < check_top_profit:
-                        trend = True
-                        txt_msg = "ขาขึ้น ☝️"
+        #     elif str(summ).find('BUY') >= 0:
+        #         if interesting == "Buy":
+        #             if last_price[1] > check_lower_profit and last_price[
+        #                     1] < check_top_profit:
+        #                 trend = True
+        #                 txt_msg = "ขาขึ้น ☝️"
 
-            msg = f"""ตลาด {exchange}({market})\nเหรียญ: {symbol}/{quotes}\nอยู่ในช่วง: {txt_msg}\nราคาล่าสุด: {price} {quotes}\nการเปลี่ยนแปลง: {last_price[1]}%\nMomentum: {momentum}"""
-            print(
-                f"[{colored(exchange, exchange_color)}]:=> {symbol} is {colored(interesting, txt_color)}({score}-{total_timeframe} = {colored(score-total_timeframe, txt_color)}) price: {colored(price, txt_color)} {quotes} percent: {colored(last_price[1], txt_color)} % avg: {colored(score, txt_color)}"
-            )
+        #     msg = f"""ตลาด {exchange}({market})\nเหรียญ: {symbol}/{quotes}\nอยู่ในช่วง: {txt_msg}\nราคาล่าสุด: {price} {quotes}\nการเปลี่ยนแปลง: {last_price[1]}%\nMomentum: {momentum}"""
+        #     print(
+        #         f"[{colored(exchange, exchange_color)}]:=> {symbol} is {colored(interesting, txt_color)}({score}-{total_timeframe} = {colored(score-total_timeframe, txt_color)}) price: {colored(price, txt_color)} {quotes} percent: {colored(last_price[1], txt_color)} % avg: {colored(score, txt_color)}"
+        #     )
 
         else:
             trend = False
@@ -161,7 +162,8 @@ class Trend:
             total_timeframe = 0
             msg = f"""Not Respone"""
 
-        if trend:###str(summ) == "STRONG_SELL" or str(summ) == "BUY":
+        txt_msg = ""
+        if trend:  ###str(summ) == "STRONG_SELL" or str(summ) == "BUY":
             #### check confirm_timeframes
             txt_trend = summ
             # if str(summ) == "BUY": txt_trend = "STRONG_SELL"
@@ -222,7 +224,8 @@ class Trend:
                 print(
                     f"TIME: {colored(str(c).ljust(15), 'red')}TREND: {colored(str(summ).ljust(15), 'red')}FILTER: {colored(str(txt_trend).ljust(15), 'red')}IS: {colored(str(summ).find(txt_trend) >= 0, 'red')}"
                 )
-
+                
+            txt_msg = f"ขาลง 👇"
             trend = False
             interesting = "Sell"
             txt_color = "red"
@@ -230,10 +233,15 @@ class Trend:
                 trend = True
                 interesting = "Buy"
                 txt_color = "green"
-            
-            txt_exchange = f"[{exchange}]"
-            print(f"EXC: {colored(str(txt_exchange).ljust(15), txt_color)}] SYMBOL: {colored(str(symbol).ljust(15), txt_color)} IS: {colored(str(interesting).ljust(15), txt_color)}")
+                txt_msg = f"ขาขึ้น ☝️"
 
+            txt_exchange = f"[{exchange}]"
+            print(
+                f"EXC: {colored(str(txt_exchange).ljust(15), txt_color)}] SYMBOL: {colored(str(symbol).ljust(15), txt_color)} IS: {colored(str(interesting).ljust(15), txt_color)}"
+            )
+        
+        price = f"{last_price[0]:,}" 
+        msg = f"""ตลาด {exchange}({market})\nเหรียญ: {symbol}/{quotes}\nอยู่ในช่วง: {txt_msg}\nราคาล่าสุด: {price} {quotes}\nการเปลี่ยนแปลง: {last_price[1]}%\nMomentum: {momentum}"""
         Logging(
             exchange=exchange,
             symbol=symbol,
